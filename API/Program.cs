3e62f827e9e2b76dee14a0279610602e03fca829
eldivuniqi <ev52271@ubt-uni.net>
2023-01-16 16:33:48 +0100

    add exception middleware

--- API/Program.cs
+++ API/Program.cs
@@ -40,10 +40,11 @@ using (var scope = app.Services.CreateScope())
 }
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
 }
 
 app.UseStatusCodePagesWithReExecute("/erros/{0}");
